41859d1d93345b2d4f43b7ed34862e4602d306e9
Dariusz <dariusz.borek@gmail.com>
2022-01-25 11:29:37 +0100

    6.2 Added calculator package and class

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,34 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
+
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie do testowania oprogramowania");
+
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        Calculator calculator = new Calculator();
+
+        if (calculator.add(2, 2) == 4) {
+            System.out.println("Calculator addition test OK");
+        } else {
+            System.out.println("Calculator addition test error!");
+        }
+
+        if (calculator.subtract(10, 5) == 5) {
+            System.out.println("Calculator subtraction test OK");
+        } else {
+            System.out.println("Calculator subtraction test error!");
+        }
     }
 }
