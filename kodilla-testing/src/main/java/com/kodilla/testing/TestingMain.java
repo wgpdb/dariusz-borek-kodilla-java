191e0a0b619e81cf83e6e1773c347c5d538a3f63
Dariusz <dariusz.borek@gmail.com>
2022-02-01 01:05:09 +0100

    6.2 Updated TestingMain

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -7,5 +7,28 @@ public class TestingMain {
 
     public static void main(String[] args) {
 
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        Calculator calculator = new Calculator();
+
+        if (calculator.add(2, 2) == 4) {
+            System.out.println("Calculator addition test OK");
+        } else {
+            System.out.println("Calculator addition test error!");
+        }
+
+        if (calculator.subtract(10, 5) == 5) {
+            System.out.println("Calculator subtraction test OK");
+        } else {
+            System.out.println("Calculator subtraction test error!");
+        }
     }
 }
