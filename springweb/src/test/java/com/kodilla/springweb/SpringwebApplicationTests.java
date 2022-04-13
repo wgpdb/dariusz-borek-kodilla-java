213dc9e277f74f3cf08b21f50194c5abbee0cc23
Dariusz <dariusz.borek@gmail.com>
2022-04-13 20:01:11 +0200

    14.4 First commit

---
+++ springweb/src/test/java/com/kodilla/springweb/SpringwebApplicationTests.java
@@ -0,0 +1,12 @@
+package com.kodilla.springweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
